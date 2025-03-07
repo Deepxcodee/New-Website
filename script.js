899fbbe3b9006bdcee0ddd94a5a6312a5dacfb50
Deepanshu <27529deepanshu.2021cse@gmail.com>
2025-03-07 08:53:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var cusr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    cusr.style.left = dets.x+"px"
+    cusr.style.top = dets.y+"px"
+    blur.style.left = dets.x-250 +"px"
+    blur.style.top = dets.y-250 + "px"
+})
+
+gsap.to("#nav",{
+    backgroundColor : "#000",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        //marker: true,
+        start:"top -10%",
+        end: "top-11%",
+        scrub: 1,
+    },
+});
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end: "top-70%",
+        scrub:2,
+    },
+});
